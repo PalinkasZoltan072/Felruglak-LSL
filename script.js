cbe092733c48f81a4e52ab0f54b820c98ba5f0d7
Palinkas.Zoltan <palinkas.zoltan@ckik.hugit config --global --listgit config --global user.email palinkas.zoltan@ckik.hu>
2025-03-26 12:24:21 +0100

    kezdeti nehezsegek

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const tomb = {};
+
+function keveres(params) {
+    for (let i= 0; i < array.length; i++) {
+
+        const element = array[i];
+        
+    }
+}
+
+function Showcards() {
+    
+}
+
+const Startbutton = document.querySelector("#kuldes");
+Startbutton.addEventListener("clikc", Showcards)
---
+++ script2.js
@@ -0,0 +1,75 @@
+const tomb = [
+    {
+        url: "kepek/macska1.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska2.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska3.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska4.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska5.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska6.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska7.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska8.png",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska9.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska10.jfif",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska11.webp",
+        felfedett: false,
+        megtalalt: false
+
+    },
+    {
+        url: "kepek/macska12.png",
+        felfedett: false,
+        megtalalt: false
+
+    }
+
+];
