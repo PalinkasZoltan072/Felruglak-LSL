000a6235c82ed27189281d07f705202791c3fc72
Palinkas.Zoltan <palinkas.zoltan@ckik.hugit config --global --listgit config --global user.email palinkas.zoltan@ckik.hu>
2025-03-31 10:35:46 +0200

    2025.03.31

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const h1 = document.querySelector("#szines"); 
+
+function szines() {
+    h1.style.color = `rgb(${randint(0, 255)}, ${randint(0, 255)}, ${randint(0, 255)})`;
+    h1.style.backgroundColor = `rgb(${randint(0, 255)}, ${randint(0, 255)}, ${randint(0, 255)})`;
+    setTimeout(updateBuek, 1000);
+}
+szines();
 
 const tomb = {};
 
@@ -9,9 +17,33 @@ function keveres(params) {
     }
 }
 
-function Showcards() {
+function ShowCards() 
+{
+    
+}
+
+function CreaterCards() {
+    const elsoszint = document.querySelector("#ul");
+    const masodik = document.querySelector("#ul");
+    const harmadikszint = document.querySelector("#ul");
+    
+
+    const ul = document.querySelector("#ul");
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+    
+}
+
+function Start() 
+{
+    CreaterCards();
+    ShowCards();
     
 }
 
 const Startbutton = document.querySelector("#kuldes");
-Startbutton.addEventListener("clikc", Showcards)
+Startbutton.addEventListener("clikc", Start)
+
+const table = document.querySelector("body");
