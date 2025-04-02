e562bb9c966d14560ff2d7f98ade53d133d90a29
Varga Csaba <v.csaba@ckik.loc>
2025-04-02 12:24:39 +0200

    haladunk

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+import { tomb } from './script2.js'; 
 const h1 = document.querySelector("#szines"); 
 
-function szines() {
-    h1.style.color = `rgb(${randint(0, 255)}, ${randint(0, 255)}, ${randint(0, 255)})`;
-    h1.style.backgroundColor = `rgb(${randint(0, 255)}, ${randint(0, 255)}, ${randint(0, 255)})`;
-    setTimeout(updateBuek, 1000);
-}
-szines();
+// function szines() {
+//     //h1.style.color = `rgb(${randint(0, 255)}, ${randint(0, 255)}, ${randint(0, 255)})`;
+//    // h1.style.backgroundColor = `rgb(${randint(0, 255)}, ${randint(0, 255)}, ${randint(0, 255)})`;
+//     setTimeout(updateBuek, 1000);
+// }
+// szines();
 
-const tomb = {};
 
-function keveres(params) {
-    for (let i= 0; i < array.length; i++) {
 
-        const element = array[i];
-        
+function keveres(tomb) {
+    console.log(tomb);
+    for (let i = tomb.length - 1; i > 0; i--) {
+        const random = Math.floor(Math.random() * (i + 1)); 
+        [tomb[i], tomb[random]] = [tomb[random], tomb[i]];  
     }
 }
 
@@ -24,26 +25,46 @@ function ShowCards()
 
 function CreaterCards() {
     const elsoszint = document.querySelector("#ul");
-    const masodik = document.querySelector("#ul");
-    const harmadikszint = document.querySelector("#ul");
-    
 
-    const ul = document.querySelector("#ul");
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
+    
+    const szintek = document.querySelectorAll('input[name="option"]:checked');
+    //ha nem választott szintet ki kell írtani (lekezelni), hogy ne álljon le hibaval 
+    let kartyakSzama = 12; 
+
+    const szint = szintek[0].value;
+    if (szint === "12") {
+        kartyakSzama = 12;  
+    } else if (szint === "18") {
+        kartyakSzama = 18;  
+    } else if (szint === "24") {
+        kartyakSzama = 24;  
     }
+    //generalas
+    for (let i = 0; i < kartyakSzama; i++) {
+        const li = document.createElement("li");
+        li.classList.add("card");   
+        const img = document.createElement("img");
+        img.src = tomb[i % tomb.length].url; //kepek azonos nagysaguva tetele! egyik megoldas https://imagecompressor.11zon.com/en/resize-jfif#google_vignette masik megoldas kepmetszoivel hogy png legen es utana egy img resizerbe vagy valamibe 
+        img.alt = `Kép ${i + 1}`; // 12 kép van de nekunk parokkal kell szamolni ahogy most van úgy nincs mindegyiknek párja
+        li.appendChild(img); 
+        elsoszint.appendChild(li);
+    }
+    keveres(tomb);
+
     
 }
 
 function Start() 
 {
+    Startbutton.removeEventListener("click", Start)
     CreaterCards();
+    
     ShowCards();
     
 }
 
 const Startbutton = document.querySelector("#kuldes");
-Startbutton.addEventListener("clikc", Start)
+Startbutton.addEventListener("click", Start)
+
 
 const table = document.querySelector("body");
